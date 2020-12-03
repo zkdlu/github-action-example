61ff3a01f92472db9c76489f1f67a6985d5deb69
nori <glee@lunasoft.co.kr>
2020-12-03 16:05:18 +0900

    add net core app

---
+++ net-core-demo/TestLib/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestLib
+{
+    public class Test
+    {
+        public static void Print(string msg)
+        {
+            Debug.WriteLine(msg);
+
+            Console.WriteLine(msg);
+        }
+    }
+}
